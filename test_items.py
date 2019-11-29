1578017bec262cbff0cf22ff3cb66d8eb7d58fa7
fat12-yu <fat12@mail.ru>
2019-11-29 13:56:00 +0400

    добавлено условие видимости кнопки

--- test_items.py
+++ test_items.py
@@ -4,11 +4,10 @@ import pytest
 import time
 from selenium.common.exceptions import NoSuchElementException
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
-
 class TestProductForm():
     def test_add_basket_button(self, browser):
         try:
-            browser.find_element_by_css_selector('button.btn-add-to-basket')
+            button = browser.find_element_by_css_selector('button.btn-add-to-basket')
         except:
             assert False, 'Button add to basket not found'
+        assert button.is_displayed(), "Button is not displayed"
