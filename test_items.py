4f4ed367e2336c5e6a40ab7192f2e686c7569a86
fat12-yu <fat12@mail.ru>
2019-11-29 13:46:24 +0400

    Не определялся язык браузера если линк вызывать из тестовой функции

--- test_items.py
+++ test_items.py
@@ -8,7 +8,6 @@ link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 
 class TestProductForm():
     def test_add_basket_button(self, browser):
-        browser.get(link)
         try:
             browser.find_element_by_css_selector('button.btn-add-to-basket')
         except:
