d667e008142f6b32bbab4747bff7638fe803b224
fat12-yu <fat12@mail.ru>
2019-11-29 13:48:54 +0400

    добавляем переход по линку

--- conftest.py
+++ conftest.py
@@ -10,12 +10,13 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    lang = request.config.getoption('language')
-    print(f'\n######################## lang: {lang}')
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    language = request.config.getoption('language')
+    print(f'\n######################## lang: {language}')
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages':language})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome(options=chrome_options)
+    browser = webdriver.Chrome(options=options)
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     yield browser
     print("\nquit browser..")
     browser.quit()
