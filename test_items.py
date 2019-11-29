8578f177fb17179640f42b01aa42a360c76b11ef
fat12-yu <fat12@mail.ru>
2019-11-29 13:08:53 +0400

    Осмысленные имена классу и процедуре проверке

--- test_items.py
+++ test_items.py
@@ -6,10 +6,10 @@ from selenium.common.exceptions import NoSuchElementException
 
 link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 
-class TestUrl():
-    def test_url(self, browser):
+class TestProductForm():
+    def test_add_basket_button(self, browser):
         browser.get(link)
         try:
             browser.find_element_by_css_selector('button.btn-add-to-basket')
         except:
-            assert False, 'Button not found'
+            assert False, 'Button add to basket not found'
